43fd7eba41705cc059400acbf3fe62a9d42a5033
bertrandmer <97544521+bertrandmer@users.noreply.github.com>
2022-02-15 21:46:52 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log("Hello World!\n==========\n");
+
+// Exercise 1 Section
+console.log("EXERCISE 1:\n==========\n");
+function plus(number) {
+    return(plusNumber) => return(plusNumber += number);
+    plus15 = plus(15);
+    console.log(plus15(10));
+
+}
